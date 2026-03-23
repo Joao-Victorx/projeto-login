619479b6a434b3d282e944a88bbe690fa1750711
Joao Victor <joaovictormarcondes28@gmail.com>
2026-03-23 17:19:59 -0300

    Validações do formulário, estilização das notificações de alert/warn, e mudança de cor no background do container do formulário

--- js/formulario.js
+++ js/formulario.js
@@ -4,35 +4,65 @@ function validacao() {
 
   if (email.trim() === "") {
     $("#input-email").notify("Insira seu E-mail", {
-      style: "custom-error",
-      className: "error",
+      style: "custom-alert",
+      className: "alertEmail",
+      autoHide: true,
+      autoHideDelay: 2000,
+    });
+    return false;
+  } else if (email.trim() !== "joao@gmail.com") {
+    $("#input-email").notify("E-mail Inválido", {
+      style: "custom-warn",
+      className: "warnEmail",
       autoHide: true,
       autoHideDelay: 2000,
     });
     return false;
   } else if (password.trim() === "") {
     $("#input-password").notify("Insira sua Senha", {
-      style: "custom-error",
-      className: "error",
+      style: "custom-alert",
+      className: "alertPassword",
       autoHide: true,
       autoHideDelay: 2000,
     });
     return false;
-  } else if (email.trim() === "joao@gmail.com" && password.trim() === "1") {
+  } else if (password.trim() !== "12345678") {
+    $("#input-password").notify("Senha Inválida", {
+      style: "custom-warn",
+      className: "warnPassword",
+      autoHide: true,
+      autoHideDelay: 2000,
+    });
+    if (password.length < 8) {
+      $("#input-password").notify(
+        "A senha deve conter no mínimo 8 caracteres",
+        {
+          style: "custom-alert",
+          className: "alertSizePassword",
+          autoHide: true,
+          autoHideDelay: 2000,
+        },
+      );
+    }
+    return false;
+  } else if (
+    email.trim() === "joao@gmail.com" &&
+    password.trim() === "12345678"
+  ) {
     $.notify("Login Realizado com Sucesso!", {
       style: "custom-success",
       className: "success",
       autoHide: true,
       autoHideDelay: 2000,
     });
-    $("#input-email").val(""); 
+    $("#input-email").val("");
     $("#input-password").val("");
     $("#remember-me").prop("checked", false);
     return false;
   }
 }
 
-$.notify.addStyle("custom-error", {
+$.notify.addStyle("custom-alert", {
   html: `
     <div>
       <i class="fas fa-exclamation-circle" style="margin-right: 10px;"></i>
@@ -40,15 +70,73 @@ $.notify.addStyle("custom-error", {
     </div>
   `,
   classes: {
-    error: {
+    alertEmail: {
       "background-color": "#FCF8E3",
-      "color": "#C29853",
-      "padding": "7px 15px",
+      color: "#C29853",
+      padding: "7px 15px",
       "border-radius": "4px",
       "font-weight": "bold",
-      "display": "flex",
+      display: "flex",
       "align-items": "center",
-      "width": "180px",
+      "font-size": "14px",
+      width: "170px",
+    },
+    alertPassword: {
+      "background-color": "#FCF8E3",
+      color: "#C29853",
+      padding: "7px 15px",
+      "border-radius": "4px",
+      "font-weight": "bold",
+      display: "flex",
+      "align-items": "center",
+      "font-size": "14px",
+      width: "170px",
+    },
+    alertSizePassword: {
+      "background-color": "#FCF8E3",
+      color: "#C29853",
+      padding: "7px 15px",
+      "border-radius": "4px",
+      "font-weight": "bold",
+      display: "flex",
+      "align-items": "center",
+      "font-size": "14px",
+      width: "350px",
+    },
+  },
+});
+
+$.notify.addStyle("custom-warn", {
+  html: `
+    <div>
+      <i class="fas fa-exclamation-triangle"></i>
+      <span data-notify-text/>
+    </div>
+    `,
+  classes: {
+    warnEmail: {
+      "background-color": "#FEE2E2",
+      color: "#991B1B",
+      padding: "7px 15px",
+      "border-radius": "4px",
+      "font-weight": "bold",
+      display: "flex",
+      "align-items": "center",
+      "font-size": "14px",
+      width: "170px",
+      gap: "10px",
+    },
+    warnPassword: {
+      "background-color": "#FEE2E2",
+      color: "#991B1B",
+      padding: "7px 15px",
+      "border-radius": "4px",
+      "font-weight": "bold",
+      display: "flex",
+      "align-items": "center",
+      "font-size": "14px",
+      width: "170px",
+      gap: "10px",
     },
   },
 });
@@ -62,24 +150,24 @@ $.notify.addStyle("custom-success", {
     `,
   classes: {
     success: {
-      "position": "fixed",
-      "top": "5%",
-      "left": "50%",
-      "transform": "translate(-50%, 0%)", // Para centralizar
-      "background": "#c4ffb8",
-      "color": "#209c1e",
+      position: "fixed",
+      top: "5%",
+      left: "50%",
+      transform: "translate(-50%, 0%)", // Para centralizar
+      background: "#c4ffb8",
+      color: "#209c1e",
       "font-size": "20px",
       "font-weight": "bold",
       "border-radius": "8px",
       "text-align": "center",
-      "gap": "10px",
-      "display": "flex",
+      gap: "10px",
+      display: "flex",
       "align-items": "center",
       "justify-content": "center",
       "z-index": "9999",
-      "width": "400px",
-      "height": "50px",
-      "animation": "cairNotificacao 0.5s ease-out",
+      width: "400px",
+      height: "50px",
+      animation: "cairNotificacao 0.5s ease-out",
     },
   },
 });
